37b9a0c2e0e2a75e7b5d71f416ec9148fbbec9f5
Dragon-Seeker <timekeeperguild@gmail.com>
2024-11-29 14:23:16 -0600

    docs: finish MenuInitializer java docs for how to implement on neoforge

--- common/src/main/java/com/aetherteam/cumulus/api/MenuInitializer.java
+++ common/src/main/java/com/aetherteam/cumulus/api/MenuInitializer.java
@@ -1,22 +1,26 @@
 package com.aetherteam.cumulus.api;
 
 /**
- * Serves as the main interface to register {@link Menu}'s to be listed with
- * the selection screen.
+ *  Serves as the main interface to register {@link Menu}'s to be listed with
+ *  the selection screen.
+ *  <br><br>
+ *  <strong>Neoforge</strong>: It is required that you add the {@link CumulusEntrypoint} to
+ *  your custom {@link MenuInitializer}.
  *
  *  <br><br>
- *  Example Entry Point within fabric.mod.json:
+ *  <strong>Fabric</strong>: It is required that you add your custom {@link MenuInitializer} to
+ *  your mods fabric.mod.json as instructed below:
  *  <pre>
- *  {@code
- *  "entrypoints": {
- *      "condensed_creative": [
- *        "the.class.path.here"
- *      ]
- *  }}
- *  </pre>
- *  <br>
- *  It is recommended that all <strong>Fabric Mods</strong> add the {@link CumulusEntrypoint}
- *  for use when on Neoforge and Connector is involved.
+ *   {@code
+ *   "entrypoints": {
+ *       "condensed_creative": [
+ *         "<the.class.path.here>"
+ *       ]
+ *   }}
+ *   </pre>
+ *   <br>
+ *   It is recommended that all <strong>Fabric Mods</strong> add the {@link CumulusEntrypoint}
+ *   for use when on Neoforge and Connector is involved.
  */
 public interface MenuInitializer {
     /**
