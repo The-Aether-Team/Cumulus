2d295f46e22c379cede8201924c6d3929ee12e46
bconlon <bconlon.gml@gmail.com>
2025-01-18 14:37:02 -0800

    fix: screen mixins

--- src/main/java/com/aetherteam/cumulus/mixin/mixins/client/MinecraftMixinHighest.java
+++ src/main/java/com/aetherteam/cumulus/mixin/mixins/client/MinecraftMixinHighest.java
@@ -16,12 +16,9 @@ public class MinecraftMixinHighest {
     /**
      * @see MenuListener#onGuiOpenHighest(Screen)
      */
-    @Inject(method = "setScreen(Lnet/minecraft/client/gui/screens/Screen;)V", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/BufferUploader;reset()V", shift = At.Shift.BEFORE),
-            slice = @Slice(
-                    from = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;shouldShowDeathScreen()Z"),
-                    to = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/BufferUploader;reset()V")))
-    public void setScreen(Screen guiScreen, CallbackInfo ci, @Local LocalRef<Screen> screen) {
-        MenuListener.onGuiOpenHighest(screen.get());
+    @Inject(method = "setScreen(Lnet/minecraft/client/gui/screens/Screen;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;added()V", shift = At.Shift.BEFORE))
+    public void setScreen(Screen guiScreen, CallbackInfo ci, @Local(argsOnly = true) Screen guiScreenRef) {
+        MenuListener.onGuiOpenHighest(guiScreenRef);
     }
 }
 
--- src/main/java/com/aetherteam/cumulus/mixin/mixins/client/MinecraftMixinLow.java
+++ src/main/java/com/aetherteam/cumulus/mixin/mixins/client/MinecraftMixinLow.java
@@ -3,12 +3,14 @@ package com.aetherteam.cumulus.mixin.mixins.client;
 import com.aetherteam.cumulus.api.MenuHelper;
 import com.aetherteam.cumulus.client.event.hooks.MenuHooks;
 import com.aetherteam.cumulus.client.event.listeners.MenuListener;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = Minecraft.class, priority = 2000)
 public class MinecraftMixinLow {
@@ -16,15 +18,11 @@ public class MinecraftMixinLow {
      * @see MenuHooks#setLastSplash(Screen, MenuHelper)
      * @see MenuHooks#trackFallbacks(Screen)
      */
-    @ModifyVariable(method = "setScreen(Lnet/minecraft/client/gui/screens/Screen;)V", at = @At("STORE"),
-            slice = @Slice(
-                    from = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;shouldShowDeathScreen()Z"),
-                    to = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/BufferUploader;reset()V")))
-    public Screen setScreen(Screen screen) {
-        Screen newScreen = MenuListener.onGuiOpenLow(screen);
+    @Inject(method = "setScreen(Lnet/minecraft/client/gui/screens/Screen;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;added()V", shift = At.Shift.BEFORE))
+    public void setScreen(Screen guiScreen, CallbackInfo ci, @Local(argsOnly = true) LocalRef<Screen> guiScreenRef) {
+        Screen newScreen = MenuListener.onGuiOpenLow(guiScreen);
         if (newScreen != null) {
-            return newScreen;
+            guiScreenRef.set(newScreen);
         }
-        return screen;
     }
 }
