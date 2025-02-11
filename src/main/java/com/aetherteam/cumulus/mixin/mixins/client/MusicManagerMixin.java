8d4e372ae47b242aebee74a7189b76ab3ba78c57
bconlon <bconlon.gml@gmail.com>
2025-02-11 13:52:04 -0800

    fix: use WrapOperation for MusicManagerMixin

--- src/main/java/com/aetherteam/cumulus/mixin/mixins/client/MusicManagerMixin.java
+++ src/main/java/com/aetherteam/cumulus/mixin/mixins/client/MusicManagerMixin.java
@@ -1,6 +1,8 @@
 package com.aetherteam.cumulus.mixin.mixins.client;
 
 import com.aetherteam.cumulus.client.CumulusClient;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.sounds.MusicManager;
 import net.minecraft.sounds.Music;
@@ -13,11 +15,11 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 public class MusicManagerMixin {
     /**
      * This mixin modifies the return of {@link Minecraft#getSituationalMusic()} as it is given to the music variable in {@link MusicManager}.
-     * @param music The original {@link Music} variable.
      * @return The modified {@link Music} variable.
      */
-    @ModifyVariable(method = "tick()V", at = @At(value = "STORE"))
-    public Music injected(Music music) {
+    @WrapOperation(method = "tick()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;getSituationalMusic()Lnet/minecraft/sounds/Music;"))
+    public Music injected(Minecraft instance, Operation<Music> original) {
+        Music music = original.call(instance);
         if (music == Musics.MENU && CumulusClient.MENU_HELPER.getActiveMusic() != null) {
             return CumulusClient.MENU_HELPER.getActiveMusic();
         }
