0a6b5c033927362afd12345b8c6e1a6735ca9fc9
bconlon <bconlon.gml@gmail.com>
2025-10-07 12:25:39 -0700

    fix: menu buttons not properly disabled by configs
    (cherry picked from commit c2c7826c1b28adbafb19fbf44ae1ed38e12e8d85)

--- common/src/main/java/com/aetherteam/cumulus/client/gui/screen/DynamicMenuButton.java
+++ common/src/main/java/com/aetherteam/cumulus/client/gui/screen/DynamicMenuButton.java
@@ -30,6 +30,8 @@ public class DynamicMenuButton extends BuilderMadeButton {
         } else {
             this.enabled = false;
         }
+        this.active = this.enabled;
+        this.visible = this.enabled;
     }
 
     private boolean shouldRender() {
@@ -51,13 +53,6 @@ public class DynamicMenuButton extends BuilderMadeButton {
             }
         }
         return offset;
-    }
-
-    @Override
-    public void onPress() {
-        if (this.enabled) {
-            super.onPress();
-        }
     }
 
     @SafeVarargs
